f5163cb24d7aef0365557349f69ae36f0ae9245b
Emmanuel Obioma <78274374+S-cloud-tech@users.noreply.github.com>
2025-01-06 19:04:21 +0100

    seat reservation

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ set -o errexit
 # Modify this line as needed for package manager
 pip install -r requiremenets.txt
 
+# Convert static files
+python manage.py collectstatic --no-input
+
 
 # Apply any outstanding database migrations
 python manage.py migrate
